372325c87e48e41555bc9983774bc2baa4a2c094
Aleksandra Marzec <olamarzec2@vp.pl>
2019-01-23 20:38:41 +0100

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by AIR on 23.01.2019.
+//  Copyright © 2019 AIR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
